ce5bd0c4083d853dadbda3b170649f28477cc8f0
Matthew Brown <matthew.e.brown.17@gmail.com>
2021-11-24 18:12:49 -0500

    Fix bug in `utils::truncate`

--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -5,8 +5,8 @@ pub fn truncate(string: &str, len: usize) -> String {
     let mut display_string = string.to_owned().clone();
 
     if display_string.len() > len {
-        display_string.truncate(usize::min(0, len - 3));
-        display_string.push_str("...");
+        display_string.truncate(usize::max(3, len));
+        if len > 3 { display_string.push_str("..."); }
     }
 
     display_string
