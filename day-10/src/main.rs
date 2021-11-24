b8c8489999c867d577344b9913ebd1ba0b7a65df
Matthew Brown <matthew.e.brown.17@gmail.com>
2021-11-24 01:19:18 -0500

    Day 10

---
+++ day-10/src/main.rs
@@ -0,0 +1,29 @@
+use utils::{cli::{parse_cli, Part}, truncate};
+use day10::run_1;
+
+fn main() {
+
+    let config = parse_cli(
+        "Elves Look, Elves Say",
+        "Advent of Code 2015, day 10"
+    );
+
+
+    for string in config.data {
+        let display = truncate(&string, 12);
+
+        let result = run_1(&string, match config.part {
+            Part::One => 40, // part one, 40 times
+            Part::Two => 50, // part two, 50 times
+        });
+
+        match result {
+            Ok(output) => println!(
+                "The output of sequence '{:>12}' is {} characters long.",
+                display, output.len()
+            ),
+            Err(e) => eprint!("Sequence '{:>12}': {}", display, e),
+        }
+    }
+
+}
