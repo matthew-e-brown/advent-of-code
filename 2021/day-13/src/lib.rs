4a27425cc4635a10e596d62bc51a902cf2f8c80b
Matthew Brown <matthew.e.brown.17@gmail.com>
2021-12-27 14:34:23 -0500

    2021 Day 13 - Add algorithm: broken; WIP

--- 2021/day-13/src/lib.rs
+++ 2021/day-13/src/lib.rs
@@ -1,4 +1,7 @@
+use std::fmt::Display;
+
 pub const LINE_ERR: &'static str = "Encountered malformed line";
+
 
 #[derive(Debug)]
 pub struct Paper {
@@ -10,34 +13,45 @@ pub struct Paper {
     dots: Vec<bool>,
 }
 
-
 impl Paper {
 
-    pub fn get(&self, pos: (usize, usize)) -> bool {
+    pub fn get(&self, pos: (usize, usize)) -> Option<&bool> {
         let (x, y) = pos;
-        self.dots[self.w * y + x]
+        self.dots.get(self.w * y + x)
     }
 
-    pub fn get_mut(&mut self, pos: (usize, usize)) -> &mut bool {
+    pub fn get_mut(&mut self, pos: (usize, usize)) -> Option<&mut bool> {
         let (x, y) = pos;
-        &mut self.dots[self.w * y + x]
+        self.dots.get_mut(self.w * y + x)
     }
 
     fn set_point(&mut self, pos: (usize, usize), val: bool) {
         let (x, y) = pos;
 
-        if x > self.w { self.w = x + 1; }
-        if y > self.h { self.h = y + 1; }
+        if x >= self.w { self.w = x + 1; }
+        if y >= self.h { self.h = y + 1; }
 
         let req_len = self.w * self.h;
         if req_len > self.dots.len() {
             self.dots.resize(req_len, false);
         }
 
-        *self.get_mut(pos) = val;
+        // Can unwrap because we just checked for overflow errors
+        *self.get_mut(pos).unwrap() = val;
     }
 
-    pub fn new<I>(data: I) -> Result<Self, String> where I: IntoIterator, I::Item: AsRef<str> {
+    /// Same as [set_point], but checks if there is already a value at that location and ORs the two
+    fn add_point(&mut self, pos: (usize, usize), val: bool) {
+        match self.get(pos) {
+            Some(&existing) => self.set_point(pos, val || existing),
+            None => self.add_point(pos, val),
+        }
+    }
+
+    pub fn new<I>(data: I) -> Result<Self, String>
+    where
+        I: IntoIterator, I::Item: AsRef<str>
+    {
 
         let mut output = Self { w: 0, h: 0, dots: Vec::new() };
 
@@ -49,24 +63,117 @@ impl Paper {
                 return Err(format!("{}: '{}'", LINE_ERR, line));
             }
 
-            let x = splits[0].parse::<usize>().or_else(|_| Err(format!("{}: '{}'", LINE_ERR, line)))?;
-            let y = splits[1].parse::<usize>().or_else(|_| Err(format!("{}: '{}'", LINE_ERR, line)))?;
+            let x = splits[0].parse().or_else(|_| Err(format!("{}: '{}'", LINE_ERR, line)))?;
+            let y = splits[1].parse().or_else(|_| Err(format!("{}: '{}'", LINE_ERR, line)))?;
 
             output.set_point((x, y), true);
         }
 
-
-        #[cfg(test)]
-        println!("Done paper parse");
-
         Ok(output)
+    }
+
+
+    pub fn fold(&mut self, fold: &Fold) -> Result<(), String> {
+
+        let invalid =  match fold.axis {
+            Axis::X => fold.position >= self.w,
+            Axis::Y => fold.position >= self.h,
+        };
+
+        if invalid {
+            return Err(format!(
+                "Can not fold along {} = {}, because paper is not large enough ({} x {})",
+                fold.axis, fold.position, self.w, self.h
+            ));
+        }
+
+        // ----
+
+        // We create a new Paper to copy the points into
+        let new_self: Paper;
+        // Finds a point's mirror point
+        let find_new_pos: fn((usize, usize), usize) -> (usize, usize);
+        // Determines if this point needs to be mirrored while copying
+        let is_after_fold: fn((usize, usize), usize) -> bool;
+
+        match fold.axis {
+            Axis::X => {
+                let (new_w, new_h) = (fold.position - 1, self.h);
+                new_self = Self { w: new_w, h: new_h, dots: Vec::new() };
+
+                find_new_pos = |p, l| (p.0 - l, p.1);
+                is_after_fold = |p, l| p.0 >= l;
+            },
+            Axis::Y => {
+                let (new_w, new_h) = (self.w, fold.position - 1);
+                new_self = Self { w: new_w, h: new_h, dots: Vec::new() };
+
+                find_new_pos = |p, l| (p.0, p.1 - l);
+                is_after_fold = |p, l| p.1 >= l;
+            },
+        }
+
+
+        for x in 0..self.w {
+            for y in 0..self.h {
+                let pos = (x, y);
+                let val = *self.get(pos).unwrap();
+
+                // This dot is before the fold-line, does not need to be mirrored; it can simply be copied over
+                if !is_after_fold((x, y), fold.position) {
+                    self.add_point(pos, val);
+                } else {
+                    let new_pos = find_new_pos(pos, fold.position);
+                    self.add_point(new_pos, val);
+                }
+            }
+        }
+
+        *self = new_self;
+        Ok(())
+    }
+
+    pub fn count(&self) -> usize {
+        self.dots.iter().fold(0, |acc, &cur| {
+            acc + if cur { 1 } else { 0 }
+        })
     }
 
 }
 
+impl Display for Paper {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for y in 0..self.h {
+            for x in 0..self.w {
+                let point = *self.get((x, y)).unwrap();
+                let dot = if point { "#" } else { "." };
+                f.write_str(dot)?;
+            }
+
+            f.write_str("\n")?;
+        }
+
+        Ok(())
+    }
+}
+
 
 #[derive(Debug)]
-pub enum Axis { X, Y }
+pub enum Axis {
+    X,
+    Y,
+}
+
+impl Display for Axis {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Axis::X => f.write_str("x"),
+            Axis::Y => f.write_str("y"),
+        }
+    }
+}
+
+
 
 #[derive(Debug)]
 pub struct Fold {
@@ -74,10 +181,12 @@ pub struct Fold {
     pub position: usize,
 }
 
-
 impl Fold {
 
-    pub fn new<I>(data: I) -> Result<Vec<Self>, String> where I: IntoIterator, I::Item: AsRef<str> {
+    pub fn new<I>(data: I) -> Result<Vec<Self>, String>
+    where
+        I: IntoIterator, I::Item: AsRef<str>
+    {
 
         let mut folds = Vec::new();
 
@@ -90,13 +199,7 @@ impl Fold {
                 .position(|c| c == '=')
                 .ok_or_else(|| format!("{}: '{}'", LINE_ERR, line))?;
 
-            #[cfg(test)]
-            println!("Found '=' at {}", e);
-
             let letter = line.chars().nth(e - 1);
-
-            #[cfg(test)]
-            println!("Letter be for 'e' = {:?}", letter);
 
             // Get the character before it
             let axis = match letter {
@@ -105,7 +208,7 @@ impl Fold {
                 _ => { return Err(format!("{}: '{}'", LINE_ERR, line)); },
             };
 
-            let position = line[e + 1..].parse().or_else(|_| Err(format!("{}: '{}'", LINE_ERR, line)))?;
+            let position = line[(e + 1)..].parse().or_else(|_| Err(format!("{}: '{}'", LINE_ERR, line)))?;
 
             folds.push(Fold { axis, position });
         }
@@ -114,6 +217,7 @@ impl Fold {
     }
 
 }
+
 
 
 pub fn parse(data: &Vec<String>) -> Result<(Paper, Vec<Fold>), String> {
@@ -153,8 +257,29 @@ mod tests {
         let data = example_data();
         let results = parse(&data);
 
-        println!("{:?}", results);
         assert!(results.is_ok());
+
+        let (mut paper, folds) = results.unwrap();
+
+        println!("{}", paper);
+        println!("{:?}", folds);
+
+        assert_eq!(paper.count(), 18);
+
+        paper.fold(&folds[0]).unwrap();
+        assert_eq!(paper.count(), 17);
+    }
+
+    #[test]
+    fn example() {
+        let data = example_data();
+        let (mut paper, folds) = parse(&data).unwrap();
+
+        for fold in folds.iter() {
+            paper.fold(fold).unwrap();
+        }
+
+        assert_eq!(paper.count(), 16);
     }
 
 }
