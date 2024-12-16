ae51d2c3ef27445beaaa2ce7dd5262d742cc3741
Matthew Brown <matthew.e.brown.17@gmail.com>
2024-12-16 00:15:13 -0500

    Complete 2024 day 1, part 2

--- 2024/day-01/src/main.rs
+++ 2024/day-01/src/main.rs
@@ -24,9 +24,36 @@ pub fn main() {
 
     // Find the differences
     let mut total = 0;
-    for (a, b) in l.into_iter().zip(r.into_iter()) {
+    for (a, b) in l.iter().zip(r.iter()) {
         total += (a - b).abs();
     }
 
-    println!("Total distance: {total}")
+    println!("Total distance (part 1): {total}");
+
+    // Both lists are already sorted, so finding the number of occurrences of each number is easy: just do a simple
+    // double-pointer scan.
+    let mut j = 0;
+    let mut sim_score = 0;
+    for x in l {
+        // Move forward until we hit a common region, skipping past anything that might not appear in `r`
+        while j < r.len() && x > r[j] {
+            j += 1;
+        }
+
+        // Count the number of times we see `x` in this region of `r` before hitting another element
+        let mut n = 0;
+        while j < r.len() && x == r[j] {
+            n += 1;
+            j += 1;
+        }
+
+        sim_score += x * n;
+
+        // Kill the outer loop early if we hit the end of `r`.
+        if !(j < r.len()) {
+            break;
+        }
+    }
+
+    println!("Similarity score (part 2): {sim_score}");
 }
