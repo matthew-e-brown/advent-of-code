20dd3cb1468ebbd47ae0fe1a5cf3dfa63fa5da65
Matthew Brown <matthew.e.brown.17@gmail.com>
2024-12-23 21:52:07 -0500

    Complete 2024 day 3, part 2

--- 2024/day-03/src/main.rs
+++ 2024/day-03/src/main.rs
@@ -4,7 +4,7 @@ use std::io::Read;
 use regex::Regex;
 
 fn main() {
-    let text = {
+    let input = {
         let path = std::env::args().skip(1).next().expect("missing input file path");
         let mut file = File::open(path).expect("failed to open file");
         let mut buf = String::new();
@@ -12,14 +12,43 @@ fn main() {
         buf
     };
 
+    println!("Sum of all mul(X,Y) expressions (part 1): {}", part1(&input));
+    println!("Sum of just the enabled mul expressions (part 2): {}", part2(&input));
+}
+
+
+fn part1(input: &str) -> u32 {
     let mut sum = 0;
 
     let mul_regex = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
-    for captures in mul_regex.captures_iter(&text) {
-        let x = captures.get(1).unwrap().as_str().parse::<u32>().expect("capture group always contains digits");
-        let y = captures.get(2).unwrap().as_str().parse::<u32>().expect("capture group always contains digits");
+    for captures in mul_regex.captures_iter(input) {
+        // Can unwrap the parsing because capture group is guaranteed to contain digits.
+        let x = captures.get(1).unwrap().as_str().parse::<u32>().unwrap();
+        let y = captures.get(2).unwrap().as_str().parse::<u32>().unwrap();
         sum += x * y;
     }
 
-    println!("Sum of all mul(X,Y) expressions: {sum}");
+    sum
+}
+
+
+fn part2(input: &str) -> u32 {
+    let mut sum = 0;
+    let mut mul_enabled = true;
+
+    let func_regex = Regex::new(r"(?<mul>mul\((\d+),(\d+)\))|(?<do>do\(\))|(?<dont>don't\(\))").unwrap();
+    for captures in func_regex.captures_iter(input) {
+        if mul_enabled && captures.name("mul").is_some() {
+            // If `mul` group exists, digits are guaranteed to.
+            let x = captures.get(2).unwrap().as_str().parse::<u32>().unwrap();
+            let y = captures.get(3).unwrap().as_str().parse::<u32>().unwrap();
+            sum += x * y;
+        } else if captures.name("do").is_some() {
+            mul_enabled = true;
+        } else if captures.name("dont").is_some() {
+            mul_enabled = false;
+        }
+    }
+
+    sum
 }
