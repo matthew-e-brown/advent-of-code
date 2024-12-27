b8b61dc81c99b40384aee4ed6abe30bf4ffb944b
Matthew Brown <matthew.e.brown.17@gmail.com>
2024-12-27 16:55:28 -0500

    Attempt 2024 day 5, part 2

--- 2024/day-05/src/main.rs
+++ 2024/day-05/src/main.rs
@@ -1,7 +1,9 @@
-mod graph;
-mod part1;
+use std::cmp::Ordering;
+use std::collections::VecDeque;
 
-use graph::PageGraph;
+use crate::graph::PageGraph;
+
+mod graph;
 
 
 fn main() {
@@ -11,10 +13,32 @@ fn main() {
     let rule_lines = lines.by_ref().take_while(|line| line.trim().len() > 0);
     let graph = PageGraph::from_input(rule_lines);
 
-    // println!("{graph:#?}");
+    let mut sorted_mid_sums = 0;
+    let mut unsorted_mid_sums = 0;
 
-    let sorted_mid_sums = part1::main(&graph, lines);
+    for line in lines {
+        let mut pages = parse_nums(line);
+        if is_ordered(&graph, &pages) {
+            sorted_mid_sums += pages[pages.len() / 2];
+        } else {
+            // This has gotta be the most inefficient way to possibly do this...
+            pages.sort_by(|a, b| {
+                // If there exists a path from a->b, then a must come before b.
+                if bfs(&graph, a, b) {
+                    Ordering::Less
+                } else if bfs(&graph, b, a) {
+                    Ordering::Greater
+                } else {
+                    Ordering::Equal
+                }
+            });
+
+            unsorted_mid_sums += pages[pages.len() / 2];
+        }
+    }
+
     println!("Sum of already-sorted middle elements (part 1): {}", sorted_mid_sums);
+    println!("Sum of freshly-sorted middle elements (part 2): {}", unsorted_mid_sums);
 }
 
 
@@ -24,4 +48,62 @@ fn parse_nums<S: AsRef<str>>(line: S) -> Vec<u32> {
         .split(',')
         .map(|n| n.parse::<u32>().expect("invalid page number"))
         .collect::<Vec<_>>()
+}
+
+
+// NB: This can be done just as easily (and more intuitively) by iterating forwards through the list. The difference
+// is that doing it this way means it can be done using just `outgoing` edges, which, depending on how part2 goes, may
+// remove the need for an
+fn is_ordered(graph: &PageGraph, pages: &[u32]) -> bool {
+    // Iterate backwards over the slice, flagging the pages that depend on those we've seen.
+    let mut flagged = Vec::new();
+    for &page_num in pages.into_iter().rev() {
+        // Check if this page appears in the list of flagged dependants, then one of its dependencies must have been
+        // found earlier in the loop (i.e., *later* in the list, since we're looping backwards).
+        if flagged.binary_search(&page_num).is_ok() {
+            return false;
+        }
+
+        // Otherwise, if this page has dependants, add them to the list for next time.
+        if let Some(page) = graph.get(&page_num) {
+            for &dep in page.outgoing() {
+                // Keep the list sorted as we insert; only insert new pages.
+                if let Err(i) = flagged.binary_search(&dep) {
+                    flagged.insert(i, dep);
+                }
+            }
+        }
+    }
+
+    // Found nothing amiss, so it's in order! :)
+    true
+}
+
+
+/// Searches the given path to see if a path exists from `src` to `dst`.
+fn bfs(graph: &PageGraph, src: &u32, dst: &u32) -> bool {
+    let Some(src) = graph.get(&src) else { return false };
+
+    let mut queue = VecDeque::new();
+    let mut enqueued = Vec::new();
+
+    queue.push_back(src);
+    enqueued.push(src.num());
+
+    while !queue.is_empty() {
+        let page = queue.pop_front().unwrap(); // unwrap: we know the queue is non-empty
+        if page.num() == *dst {
+            return true;
+        } else {
+            for &num in page.outgoing() {
+                if let Err(i) = enqueued.binary_search(&num) {
+                    let Some(page) = graph.get(&num) else { continue };
+                    queue.push_back(page);
+                    enqueued.insert(i, num);
+                }
+            }
+        }
+    }
+
+    false
 }
