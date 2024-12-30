e527d4bdcc7de4facc8fa9d4345ae4bbf72364aa
Matthew Brown <matthew.e.brown.17@gmail.com>
2024-12-30 17:15:49 -0500

    2024 day 6: convert `Cell` to newtype

--- 2024/day-06/src/main.rs
+++ 2024/day-06/src/main.rs
@@ -10,11 +10,11 @@ fn main() {
 
     let mut start_pos = None;
     let mut map = Grid::from_lines_map(input.lines(), |c, pos| match c {
-        '#' => Cell::Wall,
-        '.' => Cell::Open,
+        '#' => Cell::WALL,
+        '.' => Cell::OPEN,
         '^' => {
             start_pos = Some(pos);
-            Cell::Open
+            Cell::OPEN
         },
         _ => panic!("invalid input: unexpected char '{c}'"),
     })
@@ -27,31 +27,31 @@ fn main() {
     let mut pos = start_pos.expect("invalid input: missing start character ('^')");
     let mut dir = Direction::Up;
     let mut unique_tiles: usize = 0;
-    let mut threads_spawned: usize = 0;
 
     thread_pool.scoped(|scope| {
         loop {
             // If the cell we're currently on has *never* been visited, from any direction, count it as unique.
-            if map[pos] == Cell::Open {
+            if map[pos] == Cell::OPEN {
                 unique_tiles += 1;
             }
 
             // If this is the first time we've visited this cell while travelling in this particular direction, spawn a
             // thread to run a separate simulation to check for potential loops.
-            if map[pos].visit(dir) {
-                // In my Part 1 solution, 5444 unique tiles are visited. Our map is 130x130 2-byte tiles (enums). If we
-                // assume some tiles are visited twice in different directions, and say there are about 10,000
-                // visitations in total, then cloning this map 10,000 times will cost 130*130*2*10000 bytes or ~322 MiB
-                // in total. That's kind of a lot, but... it's not terrible, so I'm okay with this strategy.
+            if !map[pos].has_been_visited(dir) {
+                // In my Part 1 solution, 5444 unique tiles are visited. Our map is 130x130 1-byte cells. If we assume
+                // some tiles are visited twice in different directions, and say there are about 10,000 visitations in
+                // total, then cloning this map 10,000 times will cost 130*130*10000 bytes or ~161 MiB in total. That's
+                // not really that bad, so I'm okay with this strategy.
                 let map = map.clone();
                 let send = send.clone();
-                threads_spawned += 1;
                 scope.execute(move || {
                     if check_loop(pos, dir, map) {
                         send.send(pos).unwrap();
                     }
                 });
             }
+
+            map[pos].visit(dir);
 
             // Step forwards and see if we're still in-bounds.
             if !step(&mut pos, &mut dir, &map) {
@@ -66,7 +66,6 @@ fn main() {
 
     println!("Number of unique tiles encountered (part 1): {unique_tiles}");
     println!("Number of possible loops (part 2): {possible_loops}");
-    println!("(Number of threads spawned: {threads_spawned})");
 }
 
 /// Returns the tile directly "in front" of a given position, in the given direction, unless it is outside the bounds of
@@ -91,7 +90,7 @@ fn step(pos: &mut (usize, usize), dir: &mut Direction, map: &Grid<Cell>) -> bool
     loop {
         match in_front(*pos, *dir, map) {
             // If the next step is a wall, rotate and let the loop try again.
-            Some(next_pos) if map[next_pos] == Cell::Wall => dir.turn_right(),
+            Some(next_pos) if map[next_pos].is_wall() => dir.turn_right(),
             // Otherwise, step forwards to the next position.
             Some(next_pos) => {
                 *pos = next_pos;
@@ -108,62 +107,62 @@ fn step(pos: &mut (usize, usize), dir: &mut Direction, map: &Grid<Cell>) -> bool
 fn check_loop(mut pos: (usize, usize), mut dir: Direction, mut map: Grid<Cell>) -> bool {
     // Start by placing an obstacle directly in front of the current position:
     match in_front(pos, dir, &map) {
-        Some(new_pos) => map[new_pos] = Cell::Wall,
+        Some(new_pos) => map[new_pos] = Cell::WALL,
         None => return false,
     }
 
     // Now turn right and start looping to see if we eventually create a loop. The only two possible outcomes are (1) a
     // loop is created, which we can detect by seeing if we have visited a given cell before; and (2) the agent
     // eventually escapes the map.
+    map[pos].visit(dir);
     dir.turn_right();
     loop {
-        // `.visit` returns true for any *new* visitations; if we get a false, we know we've been here before.
-        if !map[pos].visit(dir) {
+        if map[pos].has_been_visited(dir) {
             break true;
-        } else if !step(&mut pos, &mut dir, &map) {
+        }
+
+        map[pos].visit(dir);
+
+        if !step(&mut pos, &mut dir, &map) {
             break false;
         }
     }
 }
 
-
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
-enum Cell {
-    /// This cell is empty and has not yet been visited.
-    Open,
-    /// This cell contains an obstacle.
-    Wall,
-    /// This cell has been visited at least once travelling in a given direction.
-    Visited(u8),
-}
+struct Cell(u8);
 
 impl Cell {
-    /// Marks the current cell as having been visited in the given direction.
-    ///
-    /// Returns `true` if this visitation is the first time the cell has been visited from this direction.
-    pub fn visit(&mut self, dir: Direction) -> bool {
-        let mask = dir.mask() & 0x0F;
-        match self {
-            Cell::Wall => panic!("cannot visit wall cell"),
-            Cell::Open => {
-                *self = Cell::Visited(mask);
-                true
-            },
-            Cell::Visited(bits) if (*bits & mask) == 0 => {
-                *bits |= mask;
-                true
-            },
-            Cell::Visited(_) => false, // Bit is already set in bitmask, don't need to re-OR
+    pub const OPEN: Cell = Self(0b0000);
+    pub const WALL: Cell = Self(0b1111);
+
+    /// Checks if this cell is a wall.
+    pub const fn is_wall(&self) -> bool {
+        self.0 == Self::WALL.0
+    }
+
+    /// Marks this cell as having been visited while facing the given direction.
+    pub const fn visit(&mut self, dir: Direction) {
+        if self.is_wall() {
+            panic!("cannot visit wall cell");
+        } else {
+            self.0 |= dir.mask();
         }
+    }
+
+    /// Checks if this cell has been visited while facing in a given direction.
+    pub const fn has_been_visited(&self, dir: Direction) -> bool {
+        !self.is_wall() && (self.0 & dir.mask()) > 0
     }
 }
 
 impl Display for Cell {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        f.write_str(match self {
-            Cell::Open => ".",
-            Cell::Wall => "#",
-            Cell::Visited(_) => "X",
+        f.write_str(match self.0 {
+            0b0000 => ".",
+            0b1111 => "#",
+            0b0001..0b1111 => "X",
+            _ => "?", // unreachable!(...unless?)
         })
     }
 }
