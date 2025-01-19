dca319fd6681ebbd90b5492db60d7eae2c68754b
Matthew Brown <matthew.e.brown.17@gmail.com>
2025-01-19 00:57:58 -0500

    Complete 2024 day 8, part 2

--- 2024/day-08/src/main.rs
+++ 2024/day-08/src/main.rs
@@ -1,82 +1,113 @@
 use std::collections::HashMap;
+use std::sync::mpsc::{self, Receiver, Sender};
+use std::thread::JoinHandle;
 
-use aoc_utils::grid::ParseGridError;
 use aoc_utils::Grid;
 
 // cspell:words antinode antinodes
 
 type Position = (usize, usize);
 
-struct Cell {
-    _antenna: Option<char>,
-    antinodes: u32,
-}
-
-struct Map {
-    grid: Grid<Cell>,
-    antennae: HashMap<char, Vec<Position>>,
-}
-
+// Went a little extra/convoluted with this one, but I wanted to have some fun with multithreading. Who knows if it
+// actually made a performance impact. Oh well! :D
 fn main() {
     let input = aoc_utils::puzzle_input();
-    let mut map = Map::new(input.lines()).unwrap();
-    let mut unique_positions = 0;
 
-    for (&_code, positions) in &map.antennae {
-        // For every antenna `a`, we want to loop over all the other antennae `b` and find the distance from `a` to `b`
-        // (as a vector). Adding that distance to `b`'s position will then give us the antinode's position.
-        for a_pos in positions {
-            for b_pos in positions {
-                if a_pos == b_pos {
-                    continue;
-                }
-
-                let &(ax, ay) = a_pos;
-                let &(bx, by) = b_pos;
-
-                // a -> b = b - a.
-                let dx = (bx as i32) - (ax as i32);
-                let dy = (by as i32) - (ay as i32);
-
-                // Careful for underflow:
-                let x = usize::try_from((bx as i32) + dx).ok();
-                let y = usize::try_from((by as i32) + dy).ok();
-                let pos = x.zip(y);
-                let cell = pos.and_then(|pos| map.grid.get_mut(pos));
-
-                if let Some(cell) = cell {
-                    if cell.antinodes == 0 {
-                        unique_positions += 1;
-                    }
-                    cell.antinodes += 1;
-                }
-            }
+    let mut antennae = HashMap::new();
+    let grid = Grid::from_lines_map(input.lines(), |char, pos| {
+        if char != '.' {
+            let v = antennae.entry(char).or_insert_with(|| Vec::new());
+            v.push(pos);
         }
-    }
 
-    println!("Unique locations with an antinode (part 1): {unique_positions}");
+        false
+    })
+    .unwrap();
+
+    let grid_size = grid.size();
+
+    let (tx1, rx1) = mpsc::channel(); // Channel for part1 positions
+    let (tx2, rx2) = mpsc::channel(); // Channel for part2 positions
+    let reducer1 = reducer(rx1, grid.clone());
+    let reducer2 = reducer(rx2, grid);
+
+    let mut pool = aoc_utils::threadpool();
+    pool.scoped(|scope| {
+        for (_code, positions) in &antennae {
+            let tx1 = tx1.clone();
+            let tx2 = tx2.clone();
+            scope.execute(move || scan_antinodes(tx1, tx2, positions, grid_size));
+        }
+    });
+
+    drop(tx1);
+    drop(tx2);
+
+    let num_pos1 = reducer1.join().unwrap();
+    let num_pos2 = reducer2.join().unwrap();
+    println!("Unique locations with an antinode, part 1 rules: {num_pos1}");
+    println!("Unique locations with an antinode, part 2 rules: {num_pos2}");
 }
 
+/// Creates an iterator over the cartesian product of a set of items, excluding self-intersection.
+fn cartesian_product<'a, T>(items: &'a [T]) -> impl Iterator<Item = (&'a T, &'a T)> + use<'a, T> {
+    items
+        .iter()
+        .flat_map(|a| items.iter().filter(|&b| !std::ptr::eq(a, b)).map(move |b| (a, b)))
+}
 
-impl Map {
-    pub fn new<I, S>(lines: I) -> Result<Map, ParseGridError>
-    where
-        I: IntoIterator<Item = S>,
-        S: AsRef<str>,
-    {
-        let mut antennae = HashMap::new();
-        let grid = Grid::from_lines_map(lines, |c, pos| {
-            let antenna = if c != '.' {
-                let v = antennae.entry(c).or_insert_with(|| Vec::new());
-                v.push(pos);
-                Some(c)
-            } else {
-                None
-            };
+fn scan_antinodes(tx1: Sender<Position>, tx2: Sender<Position>, positions: &[Position], grid_size: (usize, usize)) {
+    // Helper function to convert a signed position to an unsigned one, as well as checking for bounds within the
+    // original grid.
+    let (grid_w, grid_h) = grid_size;
+    let get_idx = |x: isize, y: isize| -> Option<(usize, usize)> {
+        let x = usize::try_from(x).ok()?;
+        let y = usize::try_from(y).ok()?;
+        (x < grid_w && y < grid_h).then_some((x, y))
+    };
 
-            Cell { _antenna: antenna, antinodes: 0 }
-        })?;
+    // For every antenna `a`, we want to loop over all the other antennae `b` and find the distance from `a` to `b` (as
+    // a vector).
+    for (a_pos, b_pos) in cartesian_product(&positions) {
+        let &(ax, ay) = a_pos;
+        let &(bx, by) = b_pos;
 
-        Ok(Map { grid, antennae })
+        // a -> b = b - a.
+        let dx = (bx as isize) - (ax as isize);
+        let dy = (by as isize) - (ay as isize);
+
+        // Part 1: Add the distance directly to `b`'s position to find the antinode's position.
+        let mut x = bx as isize;
+        let mut y = by as isize;
+        if let Some(pos) = get_idx(x + dx, y + dy) {
+            tx1.send(pos).unwrap();
+        }
+
+        // Part 2: Start at `b` and keep adding until we leave the map (see `get_idx` helper above).
+        while let Some(pos) = get_idx(x, y) {
+            tx2.send(pos).unwrap();
+            x = x + dx;
+            y = y + dy;
+        }
     }
+}
+
+/// Spawns a reducer thread that receives positions for the given [`Receiver`] and counts all of the ones which were the
+/// first on that tile.
+fn reducer(receiver: Receiver<Position>, mut grid: Grid<bool>) -> JoinHandle<u32> {
+    std::thread::spawn(move || {
+        let mut unique_positions = 0;
+
+        for pos in receiver.iter() {
+            let has_antinode = &mut grid[pos];
+
+            if !*has_antinode {
+                unique_positions += 1;
+            }
+
+            *has_antinode = true;
+        }
+
+        unique_positions
+    })
 }
