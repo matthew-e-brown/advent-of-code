512f12d32a05a1173e658828195c3fe57352cd12
Matthew Brown <matthew.e.brown.17@gmail.com>
2025-03-08 20:32:42 -0500

    2024 day 11, part 2: first attempt
    - Exponential growth is too much to handle all the way up to 75 blinks.
      It actually made Windows freak out my screen went black for a while.

--- 2024/day-11/src/main.rs
+++ 2024/day-11/src/main.rs
@@ -1,56 +1,68 @@
-use std::fmt::{Display, Write};
-
 fn main() {
     let input = aoc_utils::puzzle_input();
 
-    let mut buffer1 = input
+    let stones = input
         .split_whitespace()
-        .map(|n| n.parse::<u64>().expect("puzzle input should contain valid numbers"))
-        .collect::<Vec<_>>();
-    let mut buffer2 = Vec::with_capacity(buffer1.capacity());
+        .map(|n| n.parse::<u64>().expect("puzzle input should contain valid numbers"));
 
-    let mut in_buff = &mut buffer1;
-    let mut out_buff = &mut buffer2;
+    let mut buff1 = Vec::new();
+    let mut buff2 = Vec::new();
 
-    #[allow(unused)] // Just used for debugging.
-    let mut fmt_string = String::new();
+    let mut total_25 = 0;
+    let mut total_75 = 0;
+    for stone in stones {
+        let [s25, s75] = process_stone(stone, [25, 75], (&mut buff1, &mut buff2));
+        total_25 += s25;
+        total_75 += s75;
+    }
 
-    println!("Input: {}", fmt_list(in_buff.iter(), &mut fmt_string));
+    println!("Number of stones after blinking 25 times (part 1): {}", total_25);
+    println!("Number of stones after blinking 75 times (part 2): {}", total_75);
+}
 
-    for _blink in 1..=25 {
-        out_buff.clear();
+/// Takes a single stone and watches what happens when it is blinked at a certain number of times.
+///
+/// `checks` is an array of blink-counts at which to "check in." For each value in this array, the stones are tallied;
+/// the result of each count is returned in an array of the same length. `checks` **must** be in ascending order.
+///
+/// Buffers are borrowed from outside to avoid reallocating new buffers for each successive stone.
+fn process_stone<const N: usize>(stone: u64, checks: [u32; N], buffers: (&mut Vec<u64>, &mut Vec<u64>)) -> [usize; N] {
+    assert!(checks.is_sorted(), "checks should be in order");
 
-        for &stone in in_buff.iter() {
+    let mut r = 0;
+    let mut results = [0; N];
+    let Some(&max) = checks.last() else {
+        return [0; N]; // Only possible when N == 0.
+    };
+
+    let (mut src_buff, mut dst_buff) = buffers;
+    src_buff.clear();
+    dst_buff.clear();
+    src_buff.push(stone);
+
+    for blink in 1..=max {
+        dst_buff.clear();
+
+        for &stone in src_buff.iter() {
             if stone == 0 {
-                out_buff.push(1);
+                dst_buff.push(1);
             } else if let Some([l, r]) = split_digits(stone) {
-                out_buff.push(l);
-                out_buff.push(r);
+                dst_buff.push(l);
+                dst_buff.push(r);
             } else {
-                out_buff.push(stone * 2024);
+                dst_buff.push(stone * 2024);
             }
         }
 
-        // println!("After {_blink} blinks:\n{}", fmt_list(out_buff.iter(), &mut fmt_string));
+        if blink == checks[r] {
+            results[r] = dst_buff.len();
+            r += 1;
+        }
 
-        // Swap the two references for next time:
-        std::mem::swap(&mut in_buff, &mut out_buff);
+        std::mem::swap(&mut src_buff, &mut dst_buff);
     }
 
-    // Whichever buffer was most recently pushed into was swapped to be `in_buff` at the end of the last loop.
-    let buffer = in_buff;
-    println!("Number of stones after blinking 25 times (part 1): {}", buffer.len());
-}
-
-// For debugging. Formats a list of displayable items into a single string, without allocating a bunch of intermediate
-// strings.
-#[allow(unused)]
-fn fmt_list(items: impl Iterator<Item = impl Display>, buf: &mut String) -> &str {
-    buf.clear();
-    for x in items {
-        write!(buf, "{x} ").unwrap();
-    }
-    buf.trim_end()
+    results
 }
 
 fn split_digits(n: u64) -> Option<[u64; 2]> {
