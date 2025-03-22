daa93778bafcb252eff3867a0faa88a5b6924381
Matthew Brown <matthew.e.brown.17@gmail.com>
2025-03-21 21:47:43 -0400

    Add verbosity to CLI args, move puzzle input to static string

--- aoc_utils/src/lib.rs
+++ aoc_utils/src/lib.rs
@@ -3,9 +3,9 @@
 pub mod grid;
 
 use std::fs;
-use std::path::PathBuf;
+use std::sync::LazyLock;
 
-use clap::{Parser, ValueHint};
+use clap::{ArgAction, Parser, ValueHint};
 pub use {arrayvec, regex, scoped_threadpool};
 
 pub use self::grid::Grid;
@@ -14,17 +14,43 @@ pub use self::grid::Grid;
 #[derive(Parser, Debug)]
 struct Input {
     /// The name of the file to read puzzle input from.
-    #[arg(required = true, value_name = "FILE", value_hint = ValueHint::FilePath)]
-    path: PathBuf,
+    #[arg(
+        required = true,
+        value_name = "FILE",
+        value_hint = ValueHint::FilePath,
+        value_parser = read_file,
+    )]
+    input: String,
+
+    /// Set to activate printing. Specify multiple times for increased verbosity.
+    ///
+    /// This value is not necessarily used by all puzzles.
+    #[arg(short, long, action = ArgAction::Count)]
+    verbose: u8,
 }
 
-/// Checks program arguments on the command line for a filename and reads it to a string.
+// Clap won't accept `fs::read_to_string` directly because of a lifetime issue:
+// - https://github.com/clap-rs/clap/issues/4939
+// - https://www.reddit.com/r/rust/comments/ntqu68/implementation_of_fnonce_is_not_general_enough/
+//
+// Using a wrapper function seems to fix it.
+fn read_file(path: &str) -> std::io::Result<String> {
+    fs::read_to_string(path)
+}
+
+static CLI_INPUT: LazyLock<Input> = LazyLock::new(|| Input::parse());
+
+/// Returns the contents of the file specified as puzzle input on the command line.
 ///
 /// This implementation takes a stance and makes the upfront trade-off of favouring simplicity (reading all input into a
-/// buffer) instead of performance (operating on puzzle input as it is read from the file).
-pub fn puzzle_input() -> String {
-    let path = Input::parse().path;
-    fs::read_to_string(path).expect("failed to read puzzle input from file")
+/// static buffer) instead of performance (operating on puzzle input as it is read from the file directly).
+pub fn puzzle_input() -> &'static str {
+    &CLI_INPUT.input[..]
+}
+
+/// Checks program arguments on the command line for verbosity.
+pub fn verbosity() -> u8 {
+    CLI_INPUT.verbose
 }
 
 /// Creates a new threadpool (see [`scoped_threadpool`]).
