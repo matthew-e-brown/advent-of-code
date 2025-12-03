01d679ff1d83c39c39170330b31b6095d779b156
Matthew Brown <matthew.e.brown.17@gmail.com>
2025-12-03 13:34:22 -0500

    Complete 2025 day 2, part 2

--- 2025/day-02/src/main.rs
+++ 2025/day-02/src/main.rs
@@ -11,27 +11,37 @@ fn main() {
     // Simple scratch-space to avoid re-allocating a new string for every number:
     let mut buf = String::new();
 
-    let mut invalid_sum = 0_u64;
+    let mut invalid_sum1 = 0_u64;
+    let mut invalid_sum2 = 0_u64;
     for range in input {
         if aoc_utils::verbosity() > 0 {
             println!("Range {range:?}");
         }
 
         for id in range {
-            if !valid_id(id, &mut buf) {
+            if invalid_id1(id, &mut buf) {
                 if aoc_utils::verbosity() > 0 {
                     println!("\tID {id} is invalid.");
                 }
 
-                invalid_sum += id;
+                invalid_sum1 += id;
+            }
+
+            if invalid_id2(id, &mut buf) {
+                if aoc_utils::verbosity() > 0 {
+                    println!("\tID {id} is invalid (part 2).");
+                }
+
+                invalid_sum2 += id;
             }
         }
     }
 
-    println!("Sum of all invalid IDs (part 1): {invalid_sum}");
+    println!("Sum of all invalid IDs (part 1): {invalid_sum1}");
+    println!("Sum of all invalid IDs (part 2): {invalid_sum2}");
 }
 
-fn valid_id(id: u64, buf: &mut String) -> bool {
+fn invalid_id1(id: u64, buf: &mut String) -> bool {
     // Format the integer as a string into the buffer:
     buf.clear();
     write!(buf, "{id}").unwrap();
@@ -40,8 +50,54 @@ fn valid_id(id: u64, buf: &mut String) -> bool {
     if buf.len() % 2 == 0 {
         // If so, cut it in half. Then just check if the two are equal:
         let (a, b) = buf.split_at(buf.len() / 2);
-        a != b
+        a == b
     } else {
-        true
+        // Otherwise, the ID is valid.
+        false
     }
+}
+
+fn invalid_id2(id: u64, buf: &mut String) -> bool {
+    buf.clear();
+    write!(buf, "{id}").unwrap();
+
+    // Subsequence must be repeated at least twice to be invalid.
+    if buf.len() < 2 {
+        return false;
+    }
+
+    // We know this string is made entirely of ascii digits, since we just
+    // formatted it ourselves. So we'll work with a slice of bytes instead
+    // of UTF-8 chars.
+    let buf = buf.as_bytes();
+
+    // Since we need at least two repetitions, the repeating string can be at
+    // most half the length of the string.
+    'outer: for len in 1..=(buf.len() / 2) {
+        // We only need to bother checking slices of this length if the overall
+        // length is divisible by it; otherwise we could never fill the string
+        // fully.
+        if buf.len() % len != 0 {
+            continue;
+        }
+
+        let sub_count = buf.len() / len;
+
+        // Each sub-slice is `len` long, we have `sub_count` of them.
+        // `sub_count` is at least 2, since `len` is at most half the full length.
+        let sub1 = &buf[0..len];
+        for i in 1..sub_count {
+            let sub = &buf[len * i..len * (i + 1)];
+            if sub != sub1 {
+                continue 'outer;
+            }
+        }
+
+        // If we get through all `sub_count` sub-slices, and they all matched, then
+        // the string is invalid!
+        return true;
+    }
+
+    // If we get through all sub-slices of all lengths... string is valid.
+    false
 }
