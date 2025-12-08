97eb27b08f81698b3ebf55a42f2fab67467280c8
Matthew Brown <matthew.e.brown.17@gmail.com>
2025-12-08 00:47:30 -0500

    Complete 2025 day 7, part 2

--- 2025/day-07/src/main.rs
+++ 2025/day-07/src/main.rs
@@ -1,12 +1,29 @@
 use std::fmt::Debug;
 
-use aoc_utils::grid::{Dir4, Grid, GridIndex, Pos};
+use aoc_utils::grid::{Grid, GridIndex, Pos};
 
 #[derive(Clone, Copy, PartialEq, Eq)]
 enum Cell {
-    Empty,
-    Beam,
+    /// This cell contains empty space, but may have one or more tachyon beams passing through it.
+    Space(u64),
+    /// A tachyon beam splitter.
     Splitter,
+}
+
+impl Cell {
+    pub fn beam_count(self) -> Option<u64> {
+        match self {
+            Cell::Space(b) => Some(b),
+            Cell::Splitter => None,
+        }
+    }
+
+    pub fn beam_count_mut(&mut self) -> Option<&mut u64> {
+        match self {
+            Cell::Space(b) => Some(b),
+            Cell::Splitter => None,
+        }
+    }
 }
 
 fn main() {
@@ -19,47 +36,68 @@ fn main() {
     }
 
     // Start by setting the single cell at the starting position to a beam.
-    grid[start_pos] = Cell::Beam;
+    grid[start_pos] = Cell::Space(1);
 
     let mut splitter_hits = 0usize;
     for y in (start_pos.y() + 1)..grid.height() {
         // For each level in the grid:
         for x in 0..grid.width() {
-            let pos = (x, y);
-            // - Is there a beam above us? If so, it should extend downwards.
-            // - If we are a splitter, it should extend to our left and our right.
-            if grid[pos + Dir4::Up] == Cell::Beam {
-                if grid[pos] == Cell::Splitter {
-                    grid[pos + Dir4::Left] = Cell::Beam;
-                    grid[pos + Dir4::Right] = Cell::Beam;
-                    splitter_hits += 1;
-                } else if grid[pos] == Cell::Empty {
-                    grid[pos] = Cell::Beam;
+            // Is there a beam above us?
+            if let Cell::Space(src) = grid[(x, y - 1)]
+                && src > 0
+            {
+                // If so, then either:
+                // - This cell is a splitter, and the beam should get added to either side.
+                // - This cell is empty space, and it should get added to this cell directly.
+                match &mut grid[(x, y)] {
+                    // We want to add to however many beams are already there (i.e., from surrounding iterations)
+                    Cell::Space(cur) => *cur += src,
+                    Cell::Splitter => {
+                        splitter_hits += 1;
+                        for pos in [(x - 1, y), (x + 1, y)] {
+                            let neighbour = grid
+                                .get_mut(pos)
+                                .expect("splitters should not be against map edges")
+                                .beam_count_mut()
+                                .expect("splitters should be next to empty space");
+                            *neighbour += src;
+                        }
+                    },
                 }
             }
         }
     }
+
+    // Now, at the end, we simply need to tally up the total beams in the bottom row:
+    let total_branches = (0..grid.width())
+        .map(|x| {
+            grid[(x, grid.height() - 1)]
+                .beam_count()
+                .expect("there should be no splitters in the last row") as usize
+        })
+        .sum::<usize>();
 
     if aoc_utils::verbosity() > 0 {
         println!("\nFinal grid:");
         println!("{grid:?}");
     }
 
-    println!("Total number of tachyon beam splits (part 1): {splitter_hits}");
+    println!("Number of tachyon beam splits (part 1): {splitter_hits}");
+    println!("Number of possible timelines (part 2): {total_branches}");
 }
 
 /// Reads and validates puzzle input.
 ///
-/// Returns the grid and the initial beam position.
+/// Returns the grid and the initial beam position. Initial beam position is pre-seeded with a value of 1.
 fn parse_input(input: &str) -> (Grid<Cell>, Pos) {
     let mut start_pos = None;
 
     let grid = Grid::from_lines_map(input.lines(), |c, pos| match c {
-        '.' => Cell::Empty,
+        '.' => Cell::Space(0),
         '^' => Cell::Splitter,
         'S' if start_pos.is_none() => {
             start_pos = Some(pos);
-            Cell::Empty
+            Cell::Space(1)
         },
         'S' => panic!("invalid puzzle input: multiple 'S' chars"),
         _ => panic!("invalid puzzle input: unknown char {c}"),
@@ -78,8 +116,8 @@ impl Debug for Cell {
         const GREEN: &str = "\x1b[38;5;2m";
         const RESET: &str = "\x1b[0m";
         match self {
-            Cell::Empty => write!(f, "{BLACK}.{RESET}"),
-            Cell::Beam => write!(f, "{GREEN}|{RESET}"),
+            Cell::Space(0) => write!(f, "{BLACK}.{RESET}"),
+            Cell::Space(_) => write!(f, "{GREEN}|{RESET}"),
             Cell::Splitter => write!(f, "{WHITE}^{RESET}"),
         }
     }
