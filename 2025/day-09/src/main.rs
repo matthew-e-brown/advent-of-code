3be5c0ad9066504884401e29e37b34efa0d34305
Matthew Brown <matthew.e.brown.17@gmail.com>
2025-12-10 01:17:22 -0500

    Attempt 2025 day 9, part 2
    Works on the example, but there are a few more edge cases in the full
    puzzle input, apparently.

--- 2025/day-09/src/main.rs
+++ 2025/day-09/src/main.rs
@@ -1,41 +1,172 @@
+mod shapes;
+
+use self::shapes::{Line, Point, Polygon, Rectangle};
+
+// Attempted answers:
+// - 269731960 (too low)
+
 fn main() {
     let input = aoc_utils::puzzle_input();
-    let positions = input
+    let points = input
         .lines()
-        .map(|line| {
-            let (x, y) = line.split_once(',').expect("puzzle input lines should be comma-separated");
-            let x = x.parse::<u64>().expect("puzzle input should contain valid numbers");
-            let y = y.parse::<u64>().expect("puzzle input should contain valid numbers");
-            (x, y)
-        })
-        .collect::<Vec<_>>();
-    assert!(positions.len() >= 2, "puzzle input should have at least two points");
+        .map(|line| line.parse::<Point>().expect("Puzzle input should be valid"));
 
-    // Look... I'm kinda tired today. Sometimes, you just gotta go for the good'ole O(n²) double-for loop.
-    // (of course, don't forget that we don't need to check i=j, and we only need to check j < i or j > i).
+    // Look... I'm kinda tired today. Sometimes, you just gotta go for the good'ole fashioned O(n²) double-for loop. But
+    // that doesn't mean we can't speed things up! I'll check all possible rectangles by splitting them up into
+    // different threads.
+    let polygon = Polygon::from_points(points);
+    let all_rectangles = compute_rectangles(polygon.points());
 
-    let mut max_pair = (1, 0);
-    let mut max_area = rect_area(positions[1], positions[0]);
-    for i in 1..positions.len() {
-        for j in 0..i {
-            let area = rect_area(positions[i], positions[j]);
-            if area > max_area {
-                max_area = area;
-                max_pair = (i, j);
+    let num_threads = std::thread::available_parallelism()
+        .map(|n| n.get() / 2)
+        .unwrap_or(8)
+        .min(all_rectangles.len());
+    let job_count = all_rectangles.len() / num_threads; // All threads get at least this many jobs
+    let remainder = all_rectangles.len() % num_threads; // But the first `remainder` of them get one extra
+
+    let (max_overall, max_inside) = std::thread::scope(|scope| {
+        let polygon = &polygon;
+        let all_rectangles = &all_rectangles[..];
+
+        let mut start = 0;
+        let mut handles = Vec::with_capacity(num_threads);
+        for i in 0..num_threads {
+            let count = job_count + (i < remainder) as usize;
+            let group = &all_rectangles[start..start + count];
+
+            let handle = scope.spawn(|| find_largest_rectangles(group, polygon));
+            handles.push(handle);
+
+            start += count;
+        }
+
+        let mut max_overall = 0;
+        let mut max_inside = 0;
+        for (i, handle) in handles.into_iter().enumerate() {
+            let (overall, inside) = handle.join().expect("thread panicked");
+
+            if aoc_utils::verbosity() >= 1 {
+                println!("Thread {i} returned values ({overall}, {inside})", i = i + 1);
+            }
+
+            max_overall = max_overall.max(overall);
+            max_inside = max_inside.max(inside);
+        }
+
+        (max_overall, max_inside)
+    });
+
+    println!("Area of largest rectangle between red-tile corners (part 1): {max_overall}");
+    println!("Area when limited to just red and green tiles (part 2): {max_inside}");
+}
+
+
+/// Gets all possible unique rectangles that can be formed by a series of *n* points.
+///
+/// In total, *n²* total rectangles could be formed by *n* points. However, rectangle *i,j* and *j,i* would be
+/// duplicates; also, we can skip any *i,i* rectangles. This gives a total of *(n² - n)/2* rectangles
+/// (*∑(i=0..n)(∑(j=i+1..n)1)*).
+fn compute_rectangles(points: &[Point]) -> Vec<Rectangle> {
+    let n = points.len();
+    let mut rectangles = Vec::with_capacity((n * n - n) / 2);
+    for i in 0..n {
+        for j in i + 1..n {
+            let p1 = points[i];
+            let p2 = points[j];
+            rectangles.push(Rectangle::new(p1, p2))
+        }
+    }
+    rectangles
+}
+
+/// For a set of rectangles, finds:
+/// 1.  The largest rectangle overall.
+/// 2.  The largest rectangle that is wholly contained within the given polygon.
+fn find_largest_rectangles(rectangles: &[Rectangle], polygon: &Polygon) -> (u64, u64) {
+    let mut max_overall = 0;
+    let mut max_inside = 0;
+
+    for rect in rectangles {
+        let area = rect.area();
+
+        max_overall = max_overall.max(area);
+        if !rect_crosses_poly(rect, polygon) {
+            if aoc_utils::verbosity() >= 2 {
+                println!("Rectangle between {} and {} (area {}) fits inside polygon", rect.p1, rect.p2, area);
+            }
+
+            max_inside = max_inside.max(area);
+        } else {
+            if aoc_utils::verbosity() >= 3 {
+                println!("\tRectangle between {} and {} (area {}) does not fit inside polygon", rect.p1, rect.p2, area);
             }
         }
     }
 
-    let (i, j) = max_pair;
-    let (x1, y1) = positions[i];
-    let (x2, y2) = positions[j];
-    println!("Largest possible rectangle area (part 1): {max_area} (between {x1},{y1} and {x2},{y2})");
+    (max_overall, max_inside)
 }
 
-fn rect_area((x1, y1): (u64, u64), (x2, y2): (u64, u64)) -> u64 {
-    // Need to add one to the widths and heights of each rectangle.
-    // Two x coordinates on the same level makes a rectangle with a width of 1, not zero.
-    let w = x1.abs_diff(x2) + 1;
-    let h = y1.abs_diff(y2) + 1;
-    w * h
+/// Determines if the given rectangle "intersects" with the given polygon.
+fn rect_crosses_poly(rect: &Rectangle, polygon: &Polygon) -> bool {
+    // Starting from <https://stackoverflow.com/a/4833823/10549827>, there are a few simplifications we can make:
+    //
+    // - We know that all our rectangles are constructed entirely from points of the given polygon, so we don't need to
+    //   check if any of the rectangle's points are contained within the polygon.
+    // - Lines in this puzzle have an inherent thickness of 1 unit; we don't care when two lines *overlap*, only when
+    //   they *intersect*. Hence why this function is called `crosses` instead of `intersects`.
+    // - We know that all the lines in our puzzle are either strictly vertical or strictly horizontal.
+    // - So, the only way that two lines can "intersect" is if one is horizontal and one is vertical.
+    //
+    // That means that our rectangle is inside of our polygon as long as *none* of the edges cross.
+    let rect_lines = rect.edges();
+    for poly_line in polygon.edges() {
+        for &rect_line in &rect_lines {
+            if line_crosses_line(&rect_line, &poly_line) {
+                if aoc_utils::verbosity() >= 3 {
+                    println!("{rect_line:?} intersects {poly_line:?}");
+                }
+                return true;
+            }
+        }
+    }
+
+    false
+}
+
+fn line_crosses_line(l1: &Line, l2: &Line) -> bool {
+    // Figure out which of the two is vertical and which is horizontal:
+    // (as mentioned above, those are the only cases where an intersection can happen)
+    let (v, h) = if l1.is_vertical() && l2.is_horizontal() {
+        // | vs. --
+        (l1, l2)
+    } else if l1.is_horizontal() && l2.is_vertical() {
+        // -- vs. |
+        (l2, l1)
+    } else {
+        return false;
+    };
+
+    let (vt, vb) = if v.a.y <= v.b.y { (v.a, v.b) } else { (v.b, v.a) }; // Vertical's top/bottom
+    let (hl, hr) = if h.a.x <= h.b.x { (h.a, h.b) } else { (h.b, h.a) }; // Horizontal's left/right
+
+    // The vertical line's x position and the horizontal line's y position are both the same regardless
+    // of which start/end point we take it from:
+    let vx = v.a.x;
+    let hy = h.a.y;
+
+    // If the x coordinate of the vertical line is not between the endpoints of the horizontal line,
+    // or the y coordinate of the horizontal line is not between the endpoints of the vertical line,
+    // there is no intersection:
+    if (vx < hl.x || vx > hr.x) || (hy < vt.y || hy > vb.y) {
+        return false;
+    }
+
+    // Otherwise, if the vertical line's two y coordinates are on opposite sides of the horizontal line,
+    // or the horizontal line's x coordinates are on opposite sides of the vertical line,
+    // we have an intersection!
+    if (vt.y < hy && vb.y > hy) || (hl.x < vx && hr.x > vx) {
+        return true;
+    }
+
+    false
 }
---
+++ 2025/day-09/src/shapes.rs
@@ -0,0 +1,170 @@
+use std::fmt::{Debug, Display};
+use std::str::FromStr;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct Point {
+    pub x: u32,
+    pub y: u32,
+}
+
+#[derive(Clone, Copy, PartialEq, Eq)]
+pub struct Line {
+    pub a: Point,
+    pub b: Point,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct Rectangle {
+    pub p1: Point,
+    pub p2: Point,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Polygon {
+    /// The points that make up this polygon.
+    ///
+    /// The first point is repeated at the end of the list to make this a closed polygon.
+    points: Vec<Point>,
+}
+
+impl Point {
+    /// Creates a new point from `x` and `y` coordinates.
+    pub const fn new(x: u32, y: u32) -> Point {
+        Point { x, y }
+    }
+}
+
+impl FromStr for Point {
+    type Err = &'static str;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut bits = s.split(',');
+
+        let x = bits.next().ok_or("Point is missing x coordinate")?;
+        let y = bits.next().ok_or("Point is missing y coordinate")?;
+        if bits.next().is_some() {
+            return Err("Point contains more than 2 coordinates");
+        }
+
+        let x = x.parse().map_err(|_| "Point x is not a valid number")?;
+        let y = y.parse().map_err(|_| "Point y is not a valid number")?;
+        Ok(Point { x, y })
+    }
+}
+
+impl Display for Point {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let str = format!("{},{}", self.x, self.y);
+        f.pad(&str)
+    }
+}
+
+impl Line {
+    /// Creates a new line from start and end [points][Point].
+    pub const fn new(a: Point, b: Point) -> Line {
+        Line { a, b }
+    }
+
+    /// Checks if this line is horizontal.
+    pub const fn is_horizontal(&self) -> bool {
+        self.a.y == self.b.y
+    }
+
+    /// Checks if this line is vertical.
+    pub const fn is_vertical(&self) -> bool {
+        self.a.x == self.b.x
+    }
+}
+
+impl Debug for Line {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f,"Line({} to {})", self.a, self.b)
+    }
+}
+
+impl Rectangle {
+    /// Creates a new rectangle from two corner [points][Point].
+    pub const fn new(p1: Point, p2: Point) -> Rectangle {
+        Rectangle { p1, p2 }
+    }
+
+    pub const fn width(&self) -> u32 {
+        self.p1.x.abs_diff(self.p2.x) + 1
+    }
+
+    pub const fn height(&self) -> u32 {
+        self.p1.y.abs_diff(self.p2.y) + 1
+    }
+
+    pub const fn area(&self) -> u64 {
+        (self.width() as u64) * (self.height() as u64)
+    }
+
+    /// Returns the four edges that make up this rectangle.
+    ///
+    /// The order of the edges are: top, right, bottom, left.
+    pub const fn edges(&self) -> [Line; 4] {
+        let Point { x: x1, y: y1 } = self.p1;
+        let Point { x: x2, y: y2 } = self.p2;
+
+        let [xl, xr] = if x1 <= x2 { [x1, x2] } else { [x2, x1] };
+        let [yt, yb] = if y1 <= y2 { [y1, y2] } else { [y2, y1] };
+
+        let tl = Point::new(xl, yt);
+        let tr = Point::new(xr, yt);
+        let bl = Point::new(xl, yb);
+        let br = Point::new(xr, yb);
+
+        [
+            Line::new(tl, tr),
+            Line::new(tr, br),
+            Line::new(br, bl),
+            Line::new(bl, tl),
+        ]
+    }
+}
+
+impl Polygon {
+    /// Returns the total number of points in this polygon.
+    pub fn num_points(&self) -> usize {
+        // Without the repeated first point at the end
+        self.points.len() - 1
+    }
+
+    /// Reads a series of *x,y* points and verifies that they are in the correct order to form a polygon.
+    pub fn from_points(iter: impl IntoIterator<Item = Point>) -> Polygon {
+        let mut iter = iter.into_iter();
+
+        // Parse the first before starting the loop:
+        let p0 = iter.next().expect("Polygon should contain at least 3 points");
+        let mut points = vec![p0];
+        let mut i = 0;
+        while let Some(point) = iter.next() {
+            let prev = points[i];
+
+            if point.x != prev.x && point.y != prev.y {
+                panic!("Polygon can only be formed from 90° angles");
+            }
+
+            points.push(point);
+            i += 1;
+        }
+
+        // Close the polygon
+        points.push(p0);
+
+        assert!(points.len() >= 3, "Polygon should contain at least 3 points");
+        Polygon { points }
+    }
+
+    /// Gets a list of all the points in this polygon, with the last point repeated (to close the polygon).
+    pub fn points(&self) -> &[Point] {
+        &self.points[..self.num_points()]
+    }
+
+    /// Returns an iterator of all of the line segments that form this (closed) polygon.
+    pub fn edges(&self) -> impl Iterator<Item = Line> {
+        // NB: *not* `self.points()` -- we want the repeated final point so the iterator closes the polygon.
+        self.points.windows(2).map(|pts| Line::new(pts[0], pts[1]))
+    }
+}
