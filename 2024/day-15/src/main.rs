a21bea02defd15c7fd16698c82b397850c064cf0
Matthew Brown <matthew.e.brown.17@gmail.com>
2025-12-25 01:28:13 -0500

    Complete 2024 day 15, part 2

--- 2024/day-15/src/main.rs
+++ 2024/day-15/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::{HashSet, VecDeque};
 use std::fmt::Debug;
 
 use aoc_utils::Grid;
@@ -5,14 +6,22 @@ use aoc_utils::grid::{Dir4, Pos};
 
 fn main() {
     let input = aoc_utils::puzzle_input();
-    let (map1, pos1, moves) = parse_input(input);
-    let (map2, pos2) = widen_input(&map1, pos1);
+    let (mut map1, pos1, moves) = parse_input(input);
+    let (mut map2, pos2) = widen_input(&map1, pos1);
 
-    println!("{:#?}", [&map1 as &dyn std::fmt::Debug, &map2 as _]);
+    if aoc_utils::verbosity() >= 1 {
+        println!("Maps before simulation:\n{:#?}\n", [&map1 as &dyn std::fmt::Debug, &map2 as _]);
+    }
 
-    let gps_sum1 = simulate(map1, pos1, moves.iter().copied());
+    let gps_sum1 = simulate(&mut map1, pos1, moves.iter().copied());
+    let gps_sum2 = simulate(&mut map2, pos2, moves.iter().copied());
+
+    if aoc_utils::verbosity() >= 1 {
+        println!("Maps after simulation:\n{:#?}\n", [&map1 as &dyn std::fmt::Debug, &map2 as _]);
+    }
 
     println!("Sum of all boxes' GPS coordinates (part 1): {gps_sum1}");
+    println!("Sum of all boxes' GPS coordinates (part 2): {gps_sum2}");
 }
 
 /// A cell with a width of 1.
@@ -33,7 +42,10 @@ enum Cell2 {
 }
 
 /// Abstraction over both types of cell.
-trait Cell: Sized {
+trait Cell: Sized + Copy + Debug {
+    /// Returns a new instance of an "open" cell.
+    fn open() -> Self;
+
     /// Checks if this cell is open space.
     fn is_open(&self) -> bool;
 
@@ -43,107 +55,141 @@ trait Cell: Sized {
     /// Checks if this cell is a box.
     fn is_box(&self) -> bool;
 
-    /// If this cell is a box, returns a list of all the cells that but up against this cell in the given direction.
-    fn get_neighbours(&self, grid: &Grid<Self>, pos: Pos, dir: Dir4) -> Option<impl IntoIterator<Item = Self>>;
+    /// Checks if this cell should be included in calculating a [GPS score][gps_score].
+    fn include_in_gps(&self) -> bool;
+
+    /// If the object in this cell takes up multiple cells, this returns an iterator over all of its pieces.
+    fn all_pieces(&self, pos: Pos) -> impl IntoIterator<Item = Pos> + Clone;
 }
 
 #[rustfmt::skip]
 impl Cell for Cell1 {
+    fn open() -> Self { Cell1::Open }
+
     fn is_open(&self) -> bool { matches!(self, Self::Open) }
     fn is_wall(&self) -> bool { matches!(self, Self::Wall) }
     fn is_box(&self) -> bool { matches!(self, Self::Box) }
+    fn include_in_gps(&self) -> bool { matches!(self, Self::Box) }
 
-    fn get_neighbours(&self, grid: &Grid<Self>, pos: Pos, dir: Dir4) -> Option<impl IntoIterator<Item = Self>> {
-        // This one just has to return the single cell in that direction.
-        match self {
-            Self::Box => Some([grid[pos + dir]]),
-            Self::Open | Self::Wall => None,
-        }
+    fn all_pieces(&self, pos: Pos) -> impl IntoIterator<Item = Pos> + Clone {
+        [pos]
     }
 }
 
 #[rustfmt::skip]
 impl Cell for Cell2 {
+    fn open() -> Self { Cell2::Open }
+
     fn is_open(&self) -> bool { matches!(self, Self::Open) }
     fn is_wall(&self) -> bool { matches!(self, Self::Wall) }
     fn is_box(&self) -> bool { matches!(self, Self::BoxL | Self::BoxR) }
+    fn include_in_gps(&self) -> bool { matches!(self, Self::BoxL) }
 
-    fn get_neighbours(&self, grid: &Grid<Self>, pos: Pos, dir: Dir4) -> Option<impl IntoIterator<Item = Self>> {
-        use std::iter::FilterMap;
-        use std::array::IntoIter;
-        use Dir4::{Up, Down, Left, Right};
-
-        #[rustfmt::skip]
-        let neighbours: [Option<Self>; 2] = match (self, dir) {
-            (Self::BoxL, Dir4::Up   ) => /* Get the neighbours above and above-right */ [Some(grid[pos + Up])           , Some(grid[pos + Up + Right])  ],
-            (Self::BoxL, Dir4::Down ) => /* Get the neighbours below and below-right */ [Some(grid[pos + Down])         , Some(grid[pos + Down + Right])],
-            (Self::BoxL, Dir4::Left ) => /* Get the neighbour to the right           */ [Some(grid[pos + Left])         , None                          ],
-            (Self::BoxL, Dir4::Right) => /* Get the neighbour two to the right       */ [Some(grid[pos + Right + Right]), None                          ],
-            (Self::BoxR, Dir4::Up   ) => /* Get the neighbours above and above-left  */ [Some(grid[pos + Up])           , Some(grid[pos + Up + Left])   ],
-            (Self::BoxR, Dir4::Down ) => /* Get the neighbours below and below-left  */ [Some(grid[pos + Down])         , Some(grid[pos + Down + Left]) ],
-            (Self::BoxR, Dir4::Left ) => /* Get the neighbour two to the left        */ [Some(grid[pos + Left + Left])  , None                          ],
-            (Self::BoxR, Dir4::Right) => /* Get the neighbour to the right           */ [Some(grid[pos + Right])        , None                          ],
-            (Self::Open | Self::Wall, _) => return None,
+    fn all_pieces(&self, pos: Pos) -> impl IntoIterator<Item = Pos> + Clone {
+        let cells = match self {
+            Cell2::Open => [Some(pos), None],
+            Cell2::Wall => [Some(pos), None],
+            Cell2::BoxL => [Some(pos), Some(pos + Dir4::Right)],
+            Cell2::BoxR => [Some(pos), Some(pos + Dir4::Left)],
         };
-
-        let iter: FilterMap<IntoIter<Option<Self>, 2>, _> = neighbours.into_iter().filter_map(|n| n);
-        Some(iter)
+        cells.into_iter().filter_map(|x| x)
     }
 }
 
-fn simulate(mut map: Grid<Cell1>, start_pos: Pos, moves: impl IntoIterator<Item = Dir4>) -> usize {
+fn simulate<C: Cell>(map: &mut Grid<C>, start_pos: Pos, moves: impl IntoIterator<Item = Dir4>) -> usize {
     let mut robot_pos = start_pos;
 
     for dir in moves {
-        let in_front = map
-            .get_neighbour(robot_pos, dir)
-            .expect("robot should never reach the map border");
+        let in_front_pos = robot_pos + dir;
+        let in_front_cell = &map[in_front_pos];
 
         if aoc_utils::verbosity() >= 2 {
-            println!("Robot pos: {robot_pos:?}, dir: {dir}. Tile in front: {in_front:#?}.");
+            println!("Robot pos: {robot_pos:?}, dir: {dir}. Cell in front: {in_front_cell:#?}.");
         }
 
-        match in_front {
+        if in_front_cell.is_open() {
             // If the tile in front of us is empty, we can simply advance.
-            Cell1::Open => robot_pos += dir,
-            // If there is a wall in front of us, we do nothing.
-            Cell1::Wall => {},
-            // If there is a box in front of us, we need to check for free space on the other side of the box.
-            Cell1::Box => {
-                // Scan forward until we find a non-box, starting from the box in front of us.
-                let mut pos = robot_pos + dir;
-                while map[pos] == Cell1::Box {
-                    pos += dir;
+            robot_pos += dir;
+        } else if in_front_cell.is_box() {
+            // If there's a box in front of us, we need to check for free space on the other side of the box so we can
+            // push it. Specifically, we want to push this box and all connected boxes in the current direction, so long
+            // as none of them are blocked by a wall.
+            //
+            // To do this for boxes of arbitrary width, we basically want to do a BFS/DFS traversal (since each box may
+            // push multiple other boxes). Then, once we have completed the traversal and found that none of the boxes
+            // push up against any walls, we can push all of the boxes forwards.
+            //
+            // To actually push the boxes forwards, just like when copying in an array, we need to go in reverse:
+            // furthest away boxes need to move into the empty space, then the one closer needs to advance to take its
+            // place, etc. Otherwise, we could overwrite data before we push. To ensure we can re-process all the boxes
+            // in order, we'll use a breadth-first search, ensuring boxes are pushed onto the discovery queue in order
+            // away from the robot's position. Then, once we finish our search, we can go back over that queue
+            // backwards.
+            let mut can_push = true;
+
+            let mut bfs_order = Vec::new(); // Order we visited all boxes in our search
+            let mut bfs_frontier = VecDeque::new(); // Boxes we still need to search
+            let mut bfs_discovered = HashSet::new(); // Boxes we have discovered
+
+            let in_fronts = in_front_cell.all_pieces(in_front_pos);
+            bfs_frontier.extend(in_fronts.clone());
+            bfs_discovered.extend(in_fronts);
+
+            while let Some(pos) = bfs_frontier.pop_front() {
+                // Okay, can we push a box into this tile?
+                let cell = &map[pos];
+
+                if cell.is_wall() {
+                    // If we hit a wall at *any* point during the traversal, we can stop immediately.
+                    can_push = false;
+                    break;
+                } else if cell.is_box() {
+                    // Otherwise, this box will need to get pushed upwards.
+                    bfs_order.push(pos);
+                    let next_pos = pos + dir;
+                    let next_cell = map[next_pos];
+                    for next in next_cell.all_pieces(next_pos) {
+                        if bfs_discovered.insert(next) {
+                            bfs_frontier.push_back(next);
+                        }
+                    }
+                }
+            }
+
+            if can_push {
+                // Now we just need to actually push everything. Make sure we go backwards!
+                for pos in bfs_order.into_iter().rev() {
+                    // Note that this is not quite like shifting elements of a vector; i.e., we can't just copy the tile
+                    // from "below" us (assuming an upwards push). Consider this case:
+                    // `````````       `````````
+                    // #       #       # [][]  #
+                    // # [][]  #  ==>  #  []   #
+                    // # #[][] #       # #^ [] #
+                    // #  ^    #       #       #
+                    // `````````       `````````
+                    // Here, we don't actually want to strictly copy what was below. We need the upper two boxes to
+                    // shift away from the wall and other box they are resting above. To do that, we still need to
+                    // iterate downwards (in reverse), but we do it by moving them up and out of the way, then replacing
+                    // them with an empty spot for the next cell to move up into.
+                    assert!(map[pos].is_box(), "only boxes should have been queued for shifting");
+                    map[pos + dir] = map[pos];
+                    map[pos] = C::open();
                 }
 
-                if aoc_utils::verbosity() >= 2 {
-                    println!("\tScanned from {:?} -> {:?}. Found tile: {:#?}.", robot_pos + dir, pos, map[pos]);
-                }
-
-                // `pos` should now be the position of a non-box. If it's a wall, we can't push the stack of boxes; if
-                // it's open space, we can push the entire stack of boxes forward by one.
-                if map[pos] == Cell1::Open {
-                    // To "push the boxes forward" by one, we can simply move the first box into this open space, then
-                    // remove the one right in front of the robot.
-                    map[pos] = Cell1::Box;
-                    map[robot_pos + dir] = Cell1::Open;
-                    robot_pos += dir;
-                }
-            },
+                // Make sure we actually move the robot, too.
+                robot_pos += dir;
+            }
         }
-    }
-
-    if aoc_utils::verbosity() >= 1 {
-        println!("After moving:\n{map:?}");
     }
 
     gps_score(&map)
 }
 
 /// Computes the sum of all boxes' "GPS coordinates" in the given map.
-fn gps_score(map: &Grid<Cell1>) -> usize {
+fn gps_score<C: Cell>(map: &Grid<C>) -> usize {
     map.positions()
-        .filter_map(|(x, y)| (map[(x, y)] == Cell1::Box).then(|| x + y * 100))
+        .filter(|&pos| map[pos].include_in_gps())
+        .map(|(x, y)| x + y * 100)
         .sum()
 }
 
